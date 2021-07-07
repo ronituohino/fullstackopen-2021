a90bd85dd6d5eb8c19978bb666cf8719d34ff351
Roni Tuohino <42767842+ronituohino@users.noreply.github.com>
2021-07-07 21:17:57 +0300

    full stack!

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import numbersService from './services/numbers'
+import numbersService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
