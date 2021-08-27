af12f58747521f80b92d1fd50a81256dc04950da
Roni Tuohino <42767842+ronituohino@users.noreply.github.com>
2021-08-27 18:57:46 +0300

    9.17 done

---
+++ osa9/patientor-main/src/IndividualPatientInfo/index.tsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Header, Icon } from "semantic-ui-react";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
+
+const IndividualPatientInfo = () => {
+  const { id } = useParams<{ id: string }>();
+  const [{ patients }, dispatch] = useStateValue();
+
+  const patient = patients[id];
+
+  const fetchAllPatientInfo = async () => {
+    try {
+      const { data: patientInfo } = await axios.get<Patient>(
+        `${apiBaseUrl}/patients/${id}`
+      );
+      dispatch({ type: "UPDATE_PATIENT", payload: patientInfo });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const needToFetchInfo = (): boolean => {
+    if (!patient.ssn) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    if (needToFetchInfo()) {
+      void fetchAllPatientInfo();
+    }
+  }, [id]);
+
+  const iconName = (): string => {
+    switch (patient.gender) {
+      case "male":
+        return "mars";
+      case "female":
+        return "venus";
+      default:
+        return "neuter";
+    }
+  };
+  const iconString = iconName();
+
+  return (
+    <>
+      <Header>
+        {patient.name}
+        <Icon className={iconString}></Icon>
+      </Header>
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+    </>
+  );
+};
+
+export default IndividualPatientInfo;
---
+++ osa9/patientor-main/src/state/reducer.ts
@@ -0,0 +1,58 @@
+import { State } from "./state";
+import { Patient } from "../types";
+
+export type Action =
+  | {
+      type: "SET_PATIENT_LIST";
+      payload: Patient[];
+    }
+  | {
+      type: "ADD_PATIENT";
+      payload: Patient;
+    }
+  | {
+      type: "UPDATE_PATIENT";
+      payload: Patient;
+    };
+
+export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "SET_PATIENT_LIST":
+      return {
+        ...state,
+        patients: {
+          ...action.payload.reduce(
+            (memo, patient) => ({ ...memo, [patient.id]: patient }),
+            {}
+          ),
+          ...state.patients,
+        },
+      };
+    case "ADD_PATIENT":
+      return {
+        ...state,
+        patients: {
+          ...state.patients,
+          [action.payload.id]: action.payload,
+        },
+      };
+    case "UPDATE_PATIENT": {
+      console.log("updating patient");
+      if (Object.keys(state.patients).includes(action.payload.id)) {
+        console.log("found patient");
+        return {
+          ...state,
+          patients: {
+            ...state.patients,
+            [action.payload.id]: action.payload,
+          },
+        };
+      } else {
+        return state;
+      }
+    }
+
+    default:
+      return state;
+  }
+};
